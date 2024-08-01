7c83c984e7602dd10169b9dbf84c337f0f3e0f1b
chimoli <chimolilucy@gmail.com>
2024-08-01 16:08:45 +0300

    updates

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -2,15 +2,75 @@ import React from "react";
 import Navigation from "@/components/navigation/navigation";
 import styles from "./contact.module.css";
 import Footer from "@/components/footer/footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "@/components/header/header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact Us</h1>
-            {/* Add your contact page content here */}
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <div>
+        <Navigation />
+        <main>
+          <div className="container">
+            <h2 className={styles.h2}>contact</h2>
+            <h3 className={styles.h3}>Talk to us.</h3>
+            <div className={styles.contact_form_container}>
+              <form className={styles.contact_form}>
+                <div className={styles.form_row}>
+                  <div className={styles.form_group}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      name="firstName"
+                      required
+                    />
+                  </div>
+                  <div className={styles.form_group}>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" name="lastName" required />
+                  </div>
+                </div>
+                <div className={styles.form_row}>
+                  <div className={styles.form_group}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+                  </div>
+                  <div className={styles.form_group}>
+                    <label htmlFor="phoneNumber">Phone Number</label>
+                    <input
+                      type="tel"
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className={styles.form_row}>
+                  <div className={styles.form_group}>
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows="4"
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+                <div className={styles.form_row}>
+                  <button type="submit" className={styles.submit_button}>
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Contact;
