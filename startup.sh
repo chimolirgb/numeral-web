a10d167eaa6aaba65247b8365c745c91256838e8
chimoli <chimolilucy@gmail.com>
2024-08-28 16:16:39 +0300

    docker and .sh files

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ git pull origin master
 
 # Kill and remove the current Docker container
 echo "Killing and removing the current Docker container..."
-docker stop numeral-iot && docker rm numeral-iot
+docker stop numeral-web && docker rm numeral-web
 
 # Optionally, remove old Docker images to free up space
 echo "Removing old Docker images..."
@@ -13,10 +13,10 @@ docker image prune -a -f
 
 # Build the Docker image
 echo "Building the Docker image..."
-docker build -t numeral-iot .
+docker build -t numeral-web .
 
 # Run the Docker container
 echo "Starting the Docker container..."
-docker run --restart always -d --name numeral-iot -p 9799:3000 numeral-iot
+docker run --restart always -d --name numeral-web -p 9799:3000 numeral-web
 
 echo "Deployment completed successfully."
